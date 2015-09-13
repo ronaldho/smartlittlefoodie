570db8392bef6688dbb275066c28c697918dc210
Ronald Ho <dev@ronaldho.me>
2015-09-12 18:25:25 -0700

    Updated Git Ignore
    Worked on Storyboard and Views for multiple scenes

--- characterfeeder/InfoViewController.swift
+++ characterfeeder/InfoViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  InfoViewController.swift
 //  characterfeeder
 //
 //  Created by Ronald Ho on 2015-09-12.
@@ -8,7 +8,30 @@
 
 import UIKit
 
-class InfoViewController: UIViewController {
+class InfoViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let dataSource = [["0", "1", "2", "3", "4", "5", "6"],["0", "1", "2", "3", "4", "5", "6", "7", "8","9", "10", "11"]]
+//    let feetArray = [1...6]
+//    let inchesArray = [1...12]
+//    let inchesArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8","9", "10", "11"]
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataSource.count
+        
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return dataSource[component][row]
+    }
+//    
+//    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        <#code#>
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ characterfeeder/MealChoiceViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MealChoiceViewController.swift
+//  characterfeeder
+//
+//  Created by Ronald Ho on 2015-09-12.
+//  Copyright (c) 2015 healthyyou. All rights reserved.
+//
+
+import UIKit
+
+class MealChoiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
